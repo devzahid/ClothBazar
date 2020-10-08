6bd36420bce2434f7f3464ee7f1e82cd87ba31fa
devzahid <zsoftwaredeveloper29@gmail.com>
2020-10-08 17:46:40 +0500

    Library class

---
+++ ClothBazar.Entity/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Entity
+{
+  public  class Category:BaseEntity
+    {
+       
+        public List<Product> Products { get; set; }
+
+    }
+}
