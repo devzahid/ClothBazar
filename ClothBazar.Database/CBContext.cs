075719008162bf02005ae1543f83a4de53ca57d6
devzahid <zsoftwaredeveloper29@gmail.com>
2020-10-12 17:26:58 +0500

    Added products and category Bussines Entites and created database

---
+++ ClothBazar.Database/CBContext.cs
@@ -0,0 +1,18 @@
+﻿using ClothBazar.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Database
+{
+  public  class CBContext :DbContext
+    {
+        public CBContext() : base("ClothBAzarConnection") {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category>Categories { get; set; }
+    }
+}
