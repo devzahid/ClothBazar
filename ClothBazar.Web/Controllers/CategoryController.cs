61914fe3d7e20b4278a51d758d6323f4285b1de2
devzahid <zsoftwaredeveloper29@gmail.com>
2020-10-20 18:20:59 +0500

    Crud Control

---
+++ ClothBazar.Web/Controllers/CategoryController.cs
@@ -0,0 +1,58 @@
+﻿using ClothBazar.Entity;
+using ClothBazar.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClothBazar.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoriesServices categoryService = new CategoriesServices();
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var categories = categoryService.GetCategories();
+          //  List<Category> cats = categoryService.GetCategories();
+            return View(categories);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            categoryService.SaveCategory(category);
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Edit(int ID)
+        {
+            var category = categoryService.GetCategory(ID);
+            return View(category);
+        }
+        [HttpPost]
+        public ActionResult Edit(Category category)
+        {
+            categoryService.UpdateCategory(category);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Delete(int ID)
+        {
+            var category = categoryService.GetCategory(ID);
+            return View(category);
+        }
+        [HttpPost]
+        public ActionResult Delete(Category category)
+        {
+          //  category = categoryService.GetCategory(category.ID);
+            categoryService.DeleteCategory(category.ID);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ClothBazar.Web/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using ClothBazar.Entity;
+using ClothBazar.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClothBazar.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductsServices productsService = new ProductsServices();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ProductTable(string Search)
+        {
+            var products = productsService.GetProducts();
+            if (string.IsNullOrEmpty(Search) == false)
+            {
+                products = products.Where(p =>p.Name !=null &&  p.Name.ToLower().Contains(Search.ToLower())).ToList();
+            }
+         
+            return PartialView(products);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            productsService.SaveProduct(product);
+            return RedirectToAction("ProductTable");
+        }
+    }
+}
